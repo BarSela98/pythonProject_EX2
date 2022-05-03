86ac1a0e9403be4a8b81285ca616fe2315d47d34
Omrikre <omri.kre@gmail.com>
2022-05-03 15:17:12 +0300

    Q2

--- Q2.py
+++ Q2.py
@@ -3,17 +3,17 @@ import pandas as pd
 
 # Q2.1
 def three_x_plus_1(s):
-    lst = s.toList()
-    [x / 2 if x % 2 else (3 * x + 1) for x in lst]
-    return pd.series(lst, s.index)
+    lst = s.tolist()
+    lst = [x * 0.5 if x % 2 == 0 else (x * 3) + 1 for x in lst]
+    return pd.Series(lst, s.index)
 
 
 # Q2.2
 # check if prints well. makes series with new indexes and original data.
 def reindex_up_down(s):
     lst = s.index.toList()
-    lst = [x.upper if x[0].isupper else x.lower for x in lst]
-    return pd.series(s.data, lst)
+    lst = [x.upper if x[0].isupper() else x.lower() for x in lst]
+    return pd.series(s.toList(), lst)
 
 
 # Q2.3
@@ -29,27 +29,33 @@ def partial_sum(s):
 # Q2.5
 # empty
 def partial_eq(s1, s2):
-    #what is inters??
+    temp = [True if x == y else False for x, y in zip(s1.tolist(), s2.tolist())]
+    return pd.Series(temp, s1.index)
 
 
 # Q2.6
-# check if for 'any' its correct. need to drop col and row.
-# check if the arg is right
-def dropna_mta_style(df, how=’any’):
+def dropna_mta_style(df, how='any'):
     df.dropna(0, how)
     df.dropna(1, how)
     return df
 
 
 # Q2.7
-def get_n_largest(df, n=0, how=’col’):
-
-
+def get_n_largest(df, n=0, how='col'):
+    if how == 'row':
+        df = df.transpose()
+        df = df.sort_values(by=[0])
+    else:
+        df = df.sort_values(by=['col1'])
+    return df.iloc[n]
 
 
 # Q2.8
-def unique_dict(df, how=’col’):
+def unique_dict(df, how='col'):
+    return df.drop_duplicates(subset=[''])
 
 
 # Q2.9
-# Q2.10
+def upper(df):
+    df = df.apply(lambda s: s.astype(str).str.upper())
+    return df
